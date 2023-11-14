d81150511fa1c44cadd7e8c846712acf1a044420
Gabriel Muniz Batista <gvmb0202@gmail.com>
2023-11-14 10:37:51 -0300

    JavaScript-the-beggining

---
+++ lesson-10/lesson-10-js/10f.js
@@ -0,0 +1,17 @@
+function turnOnOff(button) {
+  button.classList.toggle('is-toggled');
+}
+
+/*
+ //const button = document.querySelector('gaming-button');
+      
+      function turnOnOff(toggleButton) {
+        const button = document.querySelector('.js-button');
+        /*if(!button.classList.contains('is-toggled')) {
+          button.classList.add('is-toggled');
+        } else {
+          button.classList.remove('is-toggled');
+        }
+
+        button.classList.toggle('is-toggled', !button.classList.contains('is-toggled'));
+      }*/
